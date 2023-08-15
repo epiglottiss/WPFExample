b7e12740ac1d746aa54b40eb9f50cfe74897192a
cus <iop245@naver.com>
2023-08-15 14:40:48 +0900

    ViewModelLocator

---
+++ WpfPrismViewModelLocator/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfPrismViewModelLocator.ViewModels;
+using WpfPrismViewModelLocator.Views;
+
+namespace WpfPrismViewModelLocator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+
+            Type viewType = typeof(MainWindow);
+            var fullname = viewType.FullName.Replace(".Views.", ".ViewModels.");
+            var assmName = viewType.Assembly.FullName;
+            var viewModelName = $"{fullname}VM, {assmName}";
+            var vm = Type.GetType(viewModelName);
+
+            //ViewModelLocationProvider.Register<MainWindow, MainWindowVM>();
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewName = viewType.FullName.Replace(".Views.", ".ViewModels.");
+                var viewAssemblyName = viewType.Assembly.FullName;
+                var viewModelName = $"{viewName}VM, {viewAssemblyName}";
+                return Type.GetType(viewModelName);
+            });
+        }
+    }
+}
---
+++ WpfPrismViewModelLocator/ViewModels/MainWindowVM.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPrismViewModelLocator.ViewModels
+{
+    public class MainWindowVM : BindableBase
+    {
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set {
+                SetProperty(ref _text, value); 
+            }
+        }
+
+        public DelegateCommand CheckCommand { get; private set; }
+
+        public MainWindowVM()
+        {
+            CheckCommand = new DelegateCommand(Check, CanCheck);
+            Text = "check VM attach";
+        }
+
+        void Check()
+        {
+            Text = "VM naming Convention";
+        }
+
+        bool CanCheck()
+        {
+            return true;
+        }
+    }
+}
