a722c37b2cd412829887d4cd69ee6cd69db13a4a
cus <iop245@naver.com>
2023-08-15 12:45:52 +0900

    ModuleExample

---
+++ ModuleExample/WpfPrismMainWindow/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfPrismModule;
+
+namespace WpfPrismMainWindow
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            var window = Container.Resolve<MainWindow>();
+            return window;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+
+            moduleCatalog.AddModule<AnyModuleModule>(InitializationMode.WhenAvailable);
+        }
+    }
+}
---
+++ ModuleExample/WpfPrismModule/AnyModuleModule.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPrismModule
+{
+    public class AnyModuleModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(Views.AnyModuleView));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
